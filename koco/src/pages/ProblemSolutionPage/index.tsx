3555d47c1c303b05e58673efaf218b0416dbf60e
hyemimi <hyemi7375@gmail.com>
2025-04-30 20:38:04 +0900

    Feat: 해설 컴포넌트 UI 개발

--- koco/src/pages/ProblemSolutionPage/index.tsx
+++ koco/src/pages/ProblemSolutionPage/index.tsx
@@ -1,13 +1,15 @@
 import BottomNav from '@/components/layout/BottomNav';
 import PageHeader from '@/components/layout/PageHeader';
 import ProblemDetailSection from './components/ProblemDetailSection';
+import ProblemSolutionSection from './components/ProblemSolutionSection';
 
 const ProblemSolutionPage = () => {
   return (
     <div>
       <PageHeader title="문제 해설" />
-      <div className="flex justify-between items-center text-sm shadow-md">
+      <div className=" shadow-md">
         <ProblemDetailSection />
+        <ProblemSolutionSection />
       </div>
       <BottomNav />
     </div>
